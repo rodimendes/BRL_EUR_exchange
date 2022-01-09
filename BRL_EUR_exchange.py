3894ae27b828897d17dc5bf871eac5ae409bd53f
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2022-01-09 16:13:05 +0000

    Salving time and quote in json format

--- BRL_EUR_exchange.py
+++ BRL_EUR_exchange.py
@@ -5,29 +5,32 @@ import datetime, time
 
 # GLOBAL CONSTANT
 CHROME_DRIVER_PATH = Service("/Users/rodrigocamila/PycharmProjects/chromedriver")
-HOUR_TO_STOP = "21:00"
+HOUR_TO_STOP = "16:10"
+GAP_TO_CHECK = datetime.timedelta(minutes=2)
 
-# INFORMATION ABOUT TIME, INTERVAL BETWEEN CHECKS AND SCRIPT CLOSING
-hour = datetime.datetime.now()
-formatted_hour = datetime.datetime.strftime(hour, "%H:%M")
-gap_to_check = datetime.timedelta(minutes=20)
-timeout = hour + gap_to_check
+# TIME TO CHECK
+check_time = datetime.datetime.now() + GAP_TO_CHECK
 
+currency_quote = {}
 
 # MAIN SCRIPT
-while formatted_hour < HOUR_TO_STOP:
-    if datetime.datetime.now() > timeout:
+while True:
+    hour = datetime.datetime.now()
+    formatted_hour = datetime.datetime.strftime(hour, "%H:%M")
+    if formatted_hour > HOUR_TO_STOP:
+        break
+    if datetime.datetime.now() > check_time:
         driver = webdriver.Chrome(service=CHROME_DRIVER_PATH)
         driver.get(url="https://www.remessaonline.com.br/")
         driver.maximize_window()
         time.sleep(3)
         cotacao = driver.find_element(By.CSS_SELECTOR, "div.amount")
-        print({cotacao.text} - {datetime.datetime.now()})
         time.sleep(5)
+        currency_quote[formatted_hour] = cotacao.text
         driver.quit()
-        timeout += gap_to_check
-        print(timeout)
+        check_time += GAP_TO_CHECK
+        print(currency_quote)
 
-print(f"Finalizando programa e cotação fechou em {cotacao.text}")
+print(f"Finalizando programa e cotação fechou em {currency_quote.keys()}")
 
 # TODO Criar lista de cotações para cada dia e alerta para determinado valor ou percentual
